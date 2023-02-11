a591e2e9e5967cb30786dd4acbf248aab085f704
Kkthnx <40672673+Kkthnx@users.noreply.github.com>
2023-02-11 12:19:20 -0500

    Translate

--- SessionPlayedTimer/core.lua
+++ SessionPlayedTimer/core.lua
@@ -3,8 +3,43 @@ local loginChecker
 
 -- Create the saved variables table
 if not SessionPlayedTimerDB then
-	SessionPlayedTimerDB = {}
+	SessionPlayedTimerDB = {
+		elapsedTime = 0,
+	}
 end
+
+-- Create a lookup table to store the translations
+local LocaleTable = {
+	-- Translations for the stranger
+	["SessionPlayedTimerTitle_deDE"] = "Session gespielt Timer",
+	["SessionPlayedTimerTitle_esES"] = "Session Jugado Timer",
+	["SessionPlayedTimerTitle_esMX"] = "Session Jugado Timer",
+	["SessionPlayedTimerTitle_frFR"] = "Session jouée Timer",
+	["SessionPlayedTimerTitle_itIT"] = "Session Giocato Timer",
+	["SessionPlayedTimerTitle_koKR"] = "세션 재생 타이머",
+	["SessionPlayedTimerTitle_ptBR"] = "Session Jogada Timer",
+	["SessionPlayedTimerTitle_ruRU"] = "Сессия воспроизведена Timer",
+	["SessionPlayedTimerTitle_zhCN"] = "会话播放计时器",
+	["SessionPlayedTimerTitle_zhTW"] = "會話播放計時器",
+	-- Translations for the desc of the addon
+	["SessionPlayedTimerInfo_deDE"] = "Anzeige der Zeit, die Sie in dieser Sitzung gespielt haben.",
+	["SessionPlayedTimerInfo_esES"] = "Muestra la cantidad de tiempo que has jugado durante esta sesión.",
+	["SessionPlayedTimerInfo_esMX"] = "Muestra la cantidad de tiempo que has jugado durante esta sesión.",
+	["SessionPlayedTimerInfo_frFR"] = "Affiche le temps que vous avez joué pendant cette session.",
+	["SessionPlayedTimerInfo_itIT"] = "Visualizza la quantità di tempo che hai giocato durante questa sessione.",
+	["SessionPlayedTimerInfo_koKR"] = "이 세션에서 플레이한 시간을 표시합니다.",
+	["SessionPlayedTimerInfo_ptBR"] = "Exibe a quantidade de tempo que você jogou durante esta sessão.",
+	["SessionPlayedTimerInfo_ruRU"] = "Отображает количество времени, которое вы провели в этой сессии.",
+	["SessionPlayedTimerInfo_zhCN"] = "显示您在此会话中游戏的时间量。",
+	["SessionPlayedTimerInfo_zhTW"] = "顯示您在此會話中遊戲的時間量。",
+}
+
+-- Retrieve the current locale and store the translation in `ThanksText`
+-- If the locale is not found in the `LocaleTable`, the default value of "Stranger" is used
+local SessionPlayerTimeTitle = LocaleTable["SessionPlayedTimerTitle_" .. GetLocale()] or "Session Played Timer"
+
+-- Retrieve the translation for the `TradeTargetInfo` text, using the current locale
+local SessionPlayerTimeInfo = LocaleTable["SessionPlayedTimerInfo_" .. GetLocale()] or "Displays the amount of time you have played during this session."
 
 -- Create a frame to display the timer
 local timerFrame = CreateFrame("Frame", "TimerFrame", UIParent)
@@ -35,8 +70,8 @@ classTexture:SetTexCoord(0.08, 0.92, 0.08, 0.92)
 timerFrame:SetScript("OnEnter", function(self)
 	if not GameTooltip:IsForbidden() then
 		GameTooltip:SetOwner(self, "ANCHOR_TOP")
-		GameTooltip:AddLine("Session Played Timer")
-		GameTooltip:AddLine("Displays the amount of time you have played during this session.")
+		GameTooltip:AddLine(SessionPlayerTimeTitle)
+		GameTooltip:AddLine(SessionPlayerTimeInfo)
 		GameTooltip:Show()
 	end
 end)
